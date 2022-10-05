4fd1b94c304e6600d20bf524e2679a5d45fa3bb0
Daniel <subiabrewd@gmail.com>
2022-10-05 02:26:06 +0200

    Refactor api functions

--- src/lib/functions/api.ts
+++ src/lib/functions/api.ts
@@ -1,37 +1,61 @@
-export interface RequestOptions
+/**
+ * The base URL of the API, e.g: `http://192.168.0.1:56903`
+ */
+export const baseUrl = [
+    'http://',
+    import.meta.env.VITE_API_ADDR,
+    ':',
+    import.meta.env.VITE_API_PORT,
+].join('');
+
+export interface RequestInitJson extends RequestInit
 {
-    body: any,
-    method?: string
-}
+    jsonBody: any
+} 
 
-export const API = {
+export const api = {
     /**
-     * The base URL of the API
+     * Perform a fetch request to the API path
+     * @param path Full path inside API, e.g: `/api/users`
+     * @param init 
+     * @returns
      */
-    address: [
-        'http://',
-        import.meta.env.VITE_API_ADDR,
-        ':',
-        import.meta.env.VITE_API_PORT,
-        '/api'
-    ].join(''),
-
-    /**
-     * Perform a fetch request to the API path using JSON
-     * @param path Must include leading slash
-     * @param request 
-     * @returns 
-     */
-    fetch: async (path: string, request?: RequestOptions) => {
-        const response = await fetch(`${API.address}${path}`, {
-            body: JSON.stringify(request?.body),
-            method: request?.method ?? 'GET',
+    fetch: async (path: string, init?: RequestInit) => {
+        return await fetch(baseUrl.concat(path), {
+            ...init,
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                'Accept': 'application/json',
+                ...init?.headers,
             }
         });
+    },
 
-        return await response.json();
-    }
+    /**
+     * Perform a GET request to the API path
+     * @param path Must inlude leading slash, e.g: `/users`
+     * @param init 'method' will be overriden to GET
+     * @returns 
+     */
+    get: async (path: string, init?: RequestInit) => {
+        return api.fetch(['/api', path].join(''), {
+            ...init,
+            method: 'GET'
+        });
+    },
+
+    /**
+     * Perform a POST request to the API path using JSON
+     * @param path Must include leading slash, e.g: `/users`
+     * @param init 'body' will be encoded as JSON, 'method' will be overriden to POST
+     * @returns 
+     */
+    post: async (path: string, init?: RequestInitJson) => {
+        return api.fetch(['/api', path].join(''), {
+            ...init,
+            body: JSON.stringify(init?.jsonBody),
+            method: 'POST'
+        });
+    },
 }
